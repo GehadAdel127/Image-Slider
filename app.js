ffe723f0169343d35f57ff3a949b6a79c519eb73
gehad <gehadadel6111@gmail.com>
2025-01-10 20:48:19 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+let items = document.querySelectorAll(".slider .list .item")
+let prev = document.getElementById("prev")
+let next = document.getElementById("next")
+let thumbnails = document.querySelectorAll(".thumbnail .item")
+let countItem = items.length
+let itemActive = 0
+
+next.onclick = function(){
+    itemActive = itemActive + 1
+    if(itemActive >= countItem){
+        itemActive = 0
+    }
+    showSlider()
+}
+prev.onclick = function(){
+    itemActive = itemActive - 1
+    if(itemActive < 0){
+        itemActive = countItem
+    }
+    showSlider()
+}
+let refreshInterval = setInterval(() => {
+    next.click()
+}, 5000)
+function showSlider(){
+    let lastActiveItem = document.querySelector(".slider .list .item.active")
+    let lastActiveThumbnail = document.querySelector(".thumbnail .item.active")
+
+    lastActiveItem.classList.remove("active")
+    lastActiveThumbnail.classList.remove("active")
+
+    items[itemActive].classList.add("active")
+    thumbnails[itemActive].classList.add("active")
+    setPositionThumbnail()
+
+    clearInterval(refreshInterval)
+    refreshInterval = setInterval(() => {
+        next.click()
+    }, 5000)
+}
+
+function setPositionThumbnail (){
+    lastActiveThumbnail.scrollIntoView({
+        behavior : "smooth",
+        block : "nearest",
+        inline : "center"
+    })
+}
+
+thumbnails.forEach((thumbnail , index) => {
+    thumbnail.addEventListener('click', function(){
+        itemActive = index
+        showSlider()
+    })
+})
